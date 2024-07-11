e565e18717b794b690969b024439226a1a00949e
anya <anya.koroliova@gmail.com>
2024-07-11 21:32:56 +0300

    5.5. Prime number check

---
+++ src/main/java/org/example/Lection5/PrimeNumber.java
@@ -0,0 +1,25 @@
+package org.example.Lection5;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.print("Введіть число: ");
+        int number = console.nextInt();
+
+        boolean result = isPrimeCheck(number);
+        System.out.println(result);
+    }
+
+    public static boolean isPrimeCheck(int n){
+        if (n < 2)
+            return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0)
+                    return false;
+
+        }
+        return true;
+    }
+}
