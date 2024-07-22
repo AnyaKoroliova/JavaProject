3289dd8663eebd7ed7a78914041fc0177495c0b0
anya <anya.koroliova@gmail.com>
2024-07-22 20:15:53 +0300

    7.1 Arrays fixed

--- src/main/java/org/example/Lection7/Arrays.java
+++ src/main/java/org/example/Lection7/Arrays.java
@@ -3,42 +3,42 @@ package org.example.Lection7;
 import java.util.Scanner;
 
 public class Arrays {
-    static int[] num = {-1, 2, 3, 4, 5};
+    static int[] number = {-1, -2, -3, -4, -5};
 
     public static double average() {
         double sum = 0;
-        for (int x: num) {
+        for (int x: number) {
             sum += x;
         }
-        return sum/num.length;
+        return sum/ number.length;
     }
 
     public static int min(){
-        int min = 0;
-        for( int i = 0; i < num.length; i++){
-            if (num[i] < min){
-                min = num[i];
+        int min = number[0];
+        for(int i = 1; i < number.length; i++){
+            if (number[i] < min){
+                min = number[i];
             }
         }
         return min;
     }
     public static int max(){
-        int max = 0;
-        for( int i = 0; i < num.length; i++){
-            if ( num[i] > max){
-                max = num[i];
+        int max = number[0];
+        for(int i = 1; i < number.length; i++){
+            if ( number[i] > max){
+                max = number[i];
             }
         }
         return max;
     }
 
     public static void arrayInReverse() {
-        for (int i = num.length - 1; i >= 0; i--) {
-            System.out.print(num[i] + " ");
+        for (int i = number.length - 1; i >= 0; i--) {
+            System.out.print(number[i] + " ");
         }
     }
-    public static boolean valExist(int inputNum){
-        for (int i : num) {
+    public static boolean valueExist(int inputNum){
+        for (int i : number) {
             if (inputNum == i) {
                 return true;
             }
@@ -49,8 +49,8 @@ public class Arrays {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         System.out.print("Введіть число: ");
-        int inputNum = console.nextInt();
-        boolean message = valExist(inputNum);
+        int inputNumber = console.nextInt();
+        boolean message = valueExist(inputNumber);
         System.out.println(message);
 
         System.out.println("Середнє значення: " + average());
