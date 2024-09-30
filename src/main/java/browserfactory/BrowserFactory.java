75db998c13e7c3ef2dea10704e6ef99603c205ad
anya <anya.koroliova@gmail.com>
2024-09-30 21:34:06 +0200

    23.1. Page object fix

--- src/main/java/browserfactory/BrowserFactory.java
+++ src/main/java/browserfactory/BrowserFactory.java
@@ -12,11 +12,10 @@ public class BrowserFactory {
 
         switch (browserType) {
             case CHROME:
-                System.setProperty("webdriver.chrome.driver", "C:\\Users\\anyak\\Downloads\\chromedriver-win64\\chromedriver.exe");
+                System.setProperty("webdriver.chrome.driver", "C:\\Users\\anyak\\Downloads\\chromedriver-win64 (1)\\chromedriver-win64\\chromedriver.exe");
                 ChromeOptions options = new ChromeOptions();
                 options.addArguments("--remote-allow-origins=*");
                 driver = new ChromeDriver();
-                //driver.manage(),window().maximize();
                 break;
 
             case FIREFOX:
