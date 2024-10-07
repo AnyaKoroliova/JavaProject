c69b8ebc33f6565aaa026f9da09200c07e4ccbb0
anya <anya.koroliova@gmail.com>
2024-10-07 21:31:48 +0300

    24.1. Page object download files fixed

--- src/test/java/pageobjecttest/FileDownloadTest.java
+++ src/test/java/pageobjecttest/FileDownloadTest.java
@@ -1,5 +1,8 @@
 package pageobjecttest;
 
+import org.example.waiters.WaitElements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import pages.GaragePage;
@@ -14,22 +17,22 @@ import java.util.List;
 
 
 public class FileDownloadTest extends TestSetup {
+    private WebDriver driver;
 
 
     @Test
     public void checkFileDownload() throws InterruptedException {
         LoginPage loginPage = new LoginPage();
         GaragePage garagePage = new GaragePage();
+        WaitElements waitElements = new WaitElements(driver);
 
         loginPage.open();
         loginPage.clickGuestLogin();
         Assert.assertEquals(getDriver().getCurrentUrl(), "https://guest:welcome2qauto@qauto.forstudy.space/panel/garage", "URL is wrong!");
 
         garagePage.clickInstructionsSideBarLink();
-        Thread.sleep(3000);
+        waitElements.waitClickableOfElement(By.xpath("//a[contains(@href, '/panel/instructions') and contains(@class, 'sidebar')]"));
         garagePage.downloadFrontWipersFileLink();
-
-        Thread.sleep(2000);
 
         File downloadedFile = new File("C:\\Users\\anyak\\Downloads\\Front windshield wipers on Audi TT.pdf");
         Assert.assertTrue(downloadedFile.exists());
@@ -48,11 +51,11 @@ public class FileDownloadTest extends TestSetup {
         garagePage.clickBrandDropdown();
         List<WebElement> brandList = garagePage.getCars();
 
-        FileWriter writer = new FileWriter("C:\\Users\\anyak\\Downloads\\brands_list.txt");
-        for (WebElement car : brandList) {
-            writer.write(car.getText() + "\n");
+        try (FileWriter writer = new FileWriter("C:\\Users\\anyak\\Downloads\\brands_list.txt")){
+            for (WebElement car : brandList) {
+                writer.write(car.getText() + "\n");
+            }
         }
-        writer.close();
 
         Assert.assertTrue(new File("C:\\Users\\anyak\\Downloads\\brands_list.txt").exists());
     }
